75f3046cd0591075f90c4ff3eb4a811b2b2e2a1f
cwon880 <cwong367@gmail.com>
2018-09-19 13:43:54 +1200

    Made record scene and set up layout of GUI components

--- src/application/MainMenuController.java
+++ src/application/MainMenuController.java
@@ -1,5 +1,25 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class MainMenuController {
+
+    @FXML
+    private Button recordBtn;
+
+    @FXML
+    private void OpenRecordScene(){
+        Parent createScene = FXMLLoader.load(getClass().getResource("RecordScene.fxml"));
+        Scene scene = new Scene(createScene);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+    }
 
 }
