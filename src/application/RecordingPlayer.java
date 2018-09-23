8734979d5854217f173de8da001c7dff0de7a91c
CWON880-SURFACE\cwong <cwong367@gmail.com>
2018-09-23 23:45:13 +1200

    Implemented compare feature, introduced RecordingPlayer class that plays audio files in background thread

---
+++ src/application/RecordingPlayer.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.concurrent.Task;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class RecordingPlayer extends Task<Void> {
+
+    private String _filePath;
+    private double _duration;
+
+    public RecordingPlayer(String filePath, double duration){
+        _filePath =filePath;
+        _duration =duration+0.5; //add another half second as record playing is not instantaneous
+    }
+
+    @Override
+    protected Void call() throws Exception {
+        InputStream inputStream = new FileInputStream(_filePath);
+        AudioStream audioStream = new AudioStream(inputStream);
+        AudioPlayer.player.start(audioStream);
+        Thread.sleep((long)_duration*1000);
+        return null;
+    }
+}
