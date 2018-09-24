67260647e704fe9d5245e84f8886478be5ec50bc
cwon880 <cwong367@gmail.com>
2018-09-24 14:04:36 +1200

    Added quitting confirmation feature

--- src/application/MainApplication.java
+++ src/application/MainApplication.java
@@ -4,7 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+
+import java.util.Optional;
 
 public class MainApplication extends Application {
 
@@ -14,6 +18,19 @@ public class MainApplication extends Application {
         primaryStage.setTitle("Name Sayer");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
+
+        primaryStage.setOnCloseRequest(e ->{ //confirmation box for exiting program
+            e.consume();
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Quit?");
+            alert.setHeaderText("Are you sure you want to exit?");
+            alert.setContentText("Hit OK to quit");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                primaryStage.close();
+            }
+        });
     }
 
 
