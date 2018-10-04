8c276035110f93bcf760e3eba97c4877f5ea38f4
cwon880 <cwong367@gmail.com>
2018-10-04 18:36:17 +1300

    Started creating custom play mode

---
+++ src/application/uiControllers/CustomModeController.java
@@ -0,0 +1,17 @@
+package application.uiControllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomModeController implements Initializable {
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- src/application/uiControllers/NamesSelectorController.java
+++ src/application/uiControllers/NamesSelectorController.java
@@ -19,17 +19,20 @@ import javafx.stage.Stage;
 
 import java.io.*;
 import java.net.URL;
-import java.util.List;
-import java.util.Optional;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class NamesSelectorController implements Initializable {
+
+    //reference for pop-up boxes https://code.makery.ch/blog/javafx-dialogs-official/
 
     @FXML
     private Button practiceBtn;
 
     @FXML
     private TextField searchBox;
+
+    @FXML
+    private ComboBox<String> customFiles;
 
     @FXML
     private ListView<String> namesList;
@@ -39,9 +42,6 @@ public class NamesSelectorController implements Initializable {
 
     @FXML
     private Button clearBtn;
-
-    @FXML
-    private Button addBtn;
 
     @FXML
     private Button makeFileBtn;
@@ -55,6 +55,8 @@ public class NamesSelectorController implements Initializable {
 
     private NamesListModel _namesListModel = new NamesListModel();
 
+    private ObservableList<String> _customFiles;
+
     @FXML
     private void goToMain(ActionEvent event) throws IOException {
         Parent listenScene = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
@@ -65,8 +67,8 @@ public class NamesSelectorController implements Initializable {
     }
 
     @FXML
-    private void goToPracticeMode(ActionEvent event) throws IOException {
-        Parent listenScene = FXMLLoader.load(getClass().getResource("PracticeMode.fxml"));
+    private void goToCustomMode(ActionEvent event) throws IOException {
+        Parent listenScene = FXMLLoader.load(getClass().getResource("CustomPlayMode.fxml"));
         Scene scene = new Scene(listenScene);
 
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -80,7 +82,7 @@ public class NamesSelectorController implements Initializable {
     }
 
     @FXML
-    private void addToSearch(MouseEvent mouseEvent){
+    private void addToSearch(MouseEvent mouseEvent){ //add selection to the search box when double clicked
         if (mouseEvent.getClickCount() == 2){
             String selection = namesList.getSelectionModel().getSelectedItem();
             if (selection != null){
@@ -93,56 +95,94 @@ public class NamesSelectorController implements Initializable {
     }
 
     @FXML
-    private void makeFile(){
+    private void makeFile(){ //ask the user to make a new custom list file
         TextInputDialog dialog = new TextInputDialog();
         dialog.setTitle("Make a new practice list!");
         dialog.setHeaderText("You are about to make a new practice list with these names");
-        dialog.setContentText("Please enter a new file name (include .txt extension):");
+        dialog.setContentText("Please enter a new file name (don't include .txt extension):");
 
         Optional<String> result = dialog.showAndWait();
         if (result.isPresent()){
-            File customFile = new File("Custom/"+result.get());
+            File customFile = new File("Custom/"+result.get()+".txt"); //check if text file user inputs is already taken
             if (customFile.exists()){
-                System.out.println("already there");
-            } else {
-                try {
-                    customFile.createNewFile();
-                    BufferedWriter bw = new BufferedWriter(new FileWriter("Custom/"+result.get(), true));
-                    PrintWriter writer = new PrintWriter(bw);
-                    List<String> customNames = selectedList.getItems();
-                    for (String name : customNames){
-                        writer.println(name);
-                    }
-                    writer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Overwrite file?");
+                alert.setHeaderText("A file already exists with this name");
+                alert.setContentText("Overwrite?");
+
+                Optional<ButtonType> entry = alert.showAndWait();
+                if (entry.get() == ButtonType.OK){
+                    customFile.delete();
+                    writeCustomFile(customFile);
                 }
+            } else {
+                writeCustomFile(customFile);
             }
         }
+    }
 
+    private void writeCustomFile(File customFile){ //write to text file by looping through selected names list and adding it to a new line of txt file
+        try {
+            customFile.createNewFile();
+            BufferedWriter bw = new BufferedWriter(new FileWriter(customFile, true));
+            PrintWriter writer = new PrintWriter(bw);
+            List<String> customNames = selectedList.getItems();
+            for (String name : customNames){
+                writer.println(name);
+            }
+            writer.close();
+            _customFiles.clear();
+            customFiles.getItems().clear();
+            getCustomFiles();
+            customFiles.getItems().addAll(_customFiles);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     private void addToSelection(KeyEvent event){
         if (event.getCode().equals(KeyCode.SPACE)){ //autocomplete by taking top result from namesList and replacing it with user entry
             String topResult = _filteredNames.get(0);
-                String currentText = searchBox.getText();
-                if (!topResult.equals("Name not found")) {
-                    if (currentText.contains(" ")) {
-                        currentText = currentText.substring(0, currentText.lastIndexOf(' '));
-                        currentText = currentText + " " + topResult;
-                        searchBox.setText(currentText);
-                        searchBox.end();
-                    } else {
-                        currentText = topResult;
-                        searchBox.setText(currentText);
-                        searchBox.endOfNextWord();
-                    }
+            String currentText = searchBox.getText();
+            if (!topResult.equals("Name not found")) {
+                if (currentText.contains(" ")) {
+                    currentText = currentText.substring(0, currentText.lastIndexOf(' '));
+                    currentText = currentText + " " + topResult;
+                    searchBox.setText(currentText);
+                    searchBox.end();
+                } else {
+                    currentText = topResult;
+                    searchBox.setText(currentText);
+                    searchBox.endOfNextWord();
                 }
+            }
         }
 
-        if (event.getCode().equals(KeyCode.ENTER)){
-            addLine(searchBox.getText());
+//        if (event.getCode().equals(KeyCode.ENTER)){
+//            addLine(searchBox.getText());
+//        }
+    }
+
+    @FXML
+    private void loadCustomFile(){ //load the current txt file in combo box onto the selected names list
+        String selectedFile = customFiles.getSelectionModel().getSelectedItem();
+        if (selectedFile != null){
+            try {
+                List<String> customNames = new ArrayList<>();
+                Scanner scanner = new Scanner(new File("Custom/"+selectedFile));
+                while (scanner.hasNextLine()) { //loop through each line and add the name to a string arraylist
+                    String line = scanner.nextLine();
+                    customNames.add(line);
+                }
+                scanner.close();
+                _selectedNames.clear(); //set the observable list to contain the string arraylist
+                _selectedNames.addAll(customNames);
+                clearBtn.setDisable(false);
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+
         }
     }
 
@@ -195,6 +235,17 @@ public class NamesSelectorController implements Initializable {
         }
     }
 
+    private void getCustomFiles(){
+        File[] files = new File("Custom").listFiles();
+        List<String> fileNames = new ArrayList<>();
+        for (File file : files){
+            if (file.isFile()){
+                fileNames.add(file.getName());
+            }
+        }
+        _customFiles = FXCollections.observableArrayList(fileNames);
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         _selectedNames = FXCollections.observableArrayList();
@@ -203,6 +254,9 @@ public class NamesSelectorController implements Initializable {
         removeBtn.setDisable(true);
         makeFileBtn.setDisable(true);
         searchBox.setPromptText("Search...");
+        getCustomFiles();
+        customFiles.setItems(_customFiles);
+
         //populate listview with names in database
         ObservableList<String> names = FXCollections.observableArrayList(_namesListModel.getNames());
         _filteredNames = new FilteredList<>(names, e -> true);
@@ -211,7 +265,7 @@ public class NamesSelectorController implements Initializable {
         searchBox.textProperty().addListener((observable,oldValue, newValue) ->{
             _filteredNames.setPredicate(element ->{
                 String currentText = newValue;
-                if (currentText.contains(" ")){
+                if (currentText.contains(" ")){ //checks if there is a space character, if there is then mark current string from last instance of space character
                     currentText = currentText.substring(currentText.lastIndexOf(' ')+1);
                 }
                 if (element.length() >= currentText.length()){
